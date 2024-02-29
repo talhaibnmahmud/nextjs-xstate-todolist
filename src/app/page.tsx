ba39068d889d2671640b7482e4998d9461852fae
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-02-29 17:45:42 +0600

    🚀 Setup: Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-svh bg-gray-950 grid place-content-center">
+    <main className="min-h-svh bg-zinc-950 grid place-content-center">
       <h1 className="text-5xl text-white font-semibold">Hello World!</h1>
     </main>
   );
